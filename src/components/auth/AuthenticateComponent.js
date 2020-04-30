e90289614c8352be5a6c5ae8cbd0299bd524c4f8
Aaquib Niaz <aaquibniaz3600@gmail.com>
2020-04-30 21:00:57 +0530

    corrected url error

--- src/components/auth/AuthenticateComponent.js
+++ src/components/auth/AuthenticateComponent.js
@@ -17,7 +17,7 @@ class AuthenticateComponent extends Component {
 
         }
         // conso
-        const url = "/auth/test"
+        const url = "http://localhost:8000/auth/test"
         try {
             const res = await axios.get(url, {
                 headers: {
